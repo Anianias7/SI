b9ab1a3b303005ea6d694830364931f0d8960b10
Anianias7 <anna.skrzeszewska95@gmail.com>
2018-03-10 21:11:01 +0100

    Poprawniona selekcja turniejowa

--- main.py
+++ main.py
@@ -9,43 +9,40 @@ from soulution import best_solution
 from swap_two_mutation import mutate
 
 
-file_name = "had20.txt"
-pop_size = 100
+file_name = "had12.dat.txt"
+pop_size = 10
 gen = 3
 Px = 70
-Pm = 0.1
-Tour = 20
+Pm = 1
+Tour = 3
 amount_of_experiments = 4
 num_of_individual_genes = file.get_number_of_cols(file_name)
 distance = file.get_distance_matrix(file_name)
 flow = file.get_flow_matrix(file_name)
 result_matrix = np.zeros((gen, 4))
 
-
 ##BASE GENERATION
 population = pop.create_population(pop_size, num_of_individual_genes)
-eval_individuals = pop.evaluate_individuals_in_population(population, distance, flow)
-best_in_generation = pop.best_in_nth_generation(eval_individuals), 0
-evaluated_generation = pop.evaluate_generation(eval_individuals, 0)
+evaluated_population = pop.evaluate_individuals_in_population(population, distance, flow)
+
+best_in_generation = pop.best_in_nth_generation(evaluated_population), 0
+evaluated_generation = pop.evaluate_generation(evaluated_population, 0)
 solution = best_in_generation
 result_matrix[0] = evaluated_generation
 
 
 for nth_gen in range(1, gen):
-    parent_population = tournament.tournament_selection(population, Tour, distance, flow)
-    parent_population = haf.crossover(parent_population, Px)
-    parent_population = mutate(parent_population, Pm)
-    evaluated_new_population = pop.evaluate_individuals_in_population(parent_population, distance, flow)
-    evaluated_generation = pop.evaluate_generation(evaluated_new_population, nth_gen)
+    print(nth_gen)
+    parent_population = tournament.tournament_selection(evaluated_population, Tour)
+    # parent_population = haf.crossover(parent_population, Px)
+    # parent_population = mutate(parent_population, Pm)
+    evaluated_generation = pop.evaluate_generation(parent_population, nth_gen)
     result_matrix[nth_gen] = evaluated_generation
-    best_in_generation = pop.best_in_nth_generation(evaluated_new_population)
+    best_in_generation = pop.best_in_nth_generation(parent_population)
     solution = best_solution(best_in_generation, solution, nth_gen)
-    population = parent_population
+    evaluated_population = parent_population
+    print('\n\n\n')
 
-print(result_matrix)
-print('\n\n\n')
-print(result_matrix[:, 1])
-print('\n\n\n')
 
 plt.xlabel('Generations')
 plt.ylabel('Costs')
--- tournament_selection.py
+++ tournament_selection.py
@@ -2,24 +2,24 @@ import random
 import population as pop
 
 
-def choose_individuals_for_tournament(population, size):
-    return random.sample(population, size)
+def choose_individuals_for_tournament(evaluated_population, size):
+    return random.sample(evaluated_population, size)
 
 
-def rate_contestants(population, size, distance_matrix, flow_matrix):
-    contestants = choose_individuals_for_tournament(population, size)
-    return list(map(lambda x: (x, pop.calculate_individual_cost(x, distance_matrix, flow_matrix)), contestants))
+# def rate_contestants(population, size, distance_matrix, flow_matrix):
+#     contestants = choose_individuals_for_tournament(population, size)
+#     return list(map(lambda x: (x, pop.calculate_individual_cost(x, distance_matrix, flow_matrix)), contestants))
 
 
-def choose_tournament_winner(population, size, distance_matrix, flow_matrix):
-    rated_contestants = rate_contestants(population, size, distance_matrix, flow_matrix)
-    best = min(rated_contestants, key=lambda rated_contestant: rated_contestants[1])
-    return best[0]
+def choose_tournament_winner(evaluated_population, size):
+    rated_contestants = choose_individuals_for_tournament(evaluated_population, size)
+    best = min(rated_contestants, key=lambda rated_contestant: rated_contestant[1])
+    return best
 
 
-def tournament_selection(population, size, distance_matrix, flow_matrix):
+def tournament_selection(evaluated_population, size):
     parents = []
-    for i in range(len(population)):
-        parents.append(choose_tournament_winner(population, size, distance_matrix, flow_matrix))
+    for i in range(len(evaluated_population)):
+        parents.append(choose_tournament_winner(evaluated_population, size))
     return parents
 
